16ee2389fb13f042fe94cd8ec15cb2e72f032d36
isabelle khan <mahk-06@hotmail.com>
2022-12-02 02:59:55 +0100

    Newfunction

--- script.js
+++ script.js
@@ -15,3 +15,12 @@ function getQuote() {
     });
 }
 
+            function hello(name){ 
+                return "Hello " + (name || "there") + "!"; 
+            } 
+            function press(){ 
+                var in = document.getElementById("input").value; 
+                var out = hello(in); 
+                document.getElementById("output").innerHTML = out; 
+            } 
+       
